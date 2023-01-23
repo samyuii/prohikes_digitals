a722d319f49aaf05c407dbb73266a6e0982993a3
Samyak <prohikes.sm@gmail.com>
2023-01-23 18:38:27 +0530

    name change

--- src/components/seo/index.jsx
+++ src/components/seo/index.jsx
@@ -7,7 +7,7 @@ const SEO = ({ title }) => {
         <Helmet>
             <meta charSet="utf-8" />
             <title>{title}</title>
-            <meta name="robots" content="noindex, follow" />
+            <meta name="robots" />
             <meta
                 name="description"
                 content="Hope – Health &amp; Medical React JS Template"
